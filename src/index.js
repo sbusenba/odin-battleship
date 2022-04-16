245bb758e02e04e8aea953d745377286aa84fb1c
Steven Busenbarrick <sbusenba@gmail.com>
2022-04-16 10:28:30 -0700

    Create gameStateIndicator and gameStates opject
    Create gameStateIndicator and gameStates object to track game state.
    Allow acceptClick to accept input for ship placement and targeting.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const battleShipDisplay = require('./battleshipdisplay');
 // setup
 
 let winner = null;
+const gameStates = ['placement',
+  'targeting',
+  'victory'];
+let gameStateIndicator = 0;
+
 const display = battleShipDisplay();
 const player1 = player('Player 1');
 const player2 = player('Computer');
@@ -16,44 +21,50 @@ let currentPlayerShots = 0;
 let opponentShots = 0;
 player1.board.placeAllShips();
 player2.board.placeAllShips();
-
+gameStateIndicator = 1;
 function acceptClick() {
-  opponent.board.queueAttack(parseInt(this.getAttribute('data-x'), 10), parseInt(this.getAttribute('data-y'), 10));
-  queuedAttacks += 1;
+  switch (gameStateIndicator) {
+    case 1:
+      opponent.board.queueAttack(parseInt(this.getAttribute('data-x'), 10), parseInt(this.getAttribute('data-y'), 10));
+      queuedAttacks += 1;
 
-  while (board1.firstElementChild) {
-    board1.removeChild(board1.firstChild);
+      while (board1.firstElementChild) {
+        board1.removeChild(board1.firstChild);
+      }
+      while (board2.firstElementChild) {
+        board2.removeChild(board2.firstChild);
+      }
+      if (queuedAttacks === currentPlayerShots) {
+        opponent.board.applyQueuedAttacks();
+        opponent.volley(currentPlayer.board, opponentShots);
+        queuedAttacks = 0;
+      }
+
+      board2.appendChild(display.render(player1.board, 'friendly', acceptClick));
+      board1.appendChild(display.render(player2.board, 'enemy', acceptClick));
+
+      currentPlayerShots = 0;
+      opponentShots = 0;
+
+      currentPlayer.board.ships.forEach((ship) => {
+        if (!ship.isSunk()) {
+          currentPlayerShots += 1;
+        }
+      });
+      opponent.board.ships.forEach((ship) => {
+        if (!ship.isSunk()) {
+          opponentShots += 1;
+        }
+      });
+      break;
+    default:
   }
-  while (board2.firstElementChild) {
-    board2.removeChild(board2.firstChild);
-  }
-  if (queuedAttacks === currentPlayerShots) {
-    opponent.board.applyQueuedAttacks();
-    opponent.volley(currentPlayer.board, opponentShots);
-    queuedAttacks = 0;
-  }
-
-  board2.appendChild(display.render(player1.board, 'friendly', acceptClick));
-  board1.appendChild(display.render(player2.board, 'enemy', acceptClick));
-
-  currentPlayerShots = 0;
-  opponentShots = 0;
-
-  currentPlayer.board.ships.forEach((ship) => {
-    if (!ship.isSunk()) {
-      currentPlayerShots += 1;
-    }
-  });
-  opponent.board.ships.forEach((ship) => {
-    if (!ship.isSunk()) {
-      opponentShots += 1;
-    }
-  });
-
   if (player1.board.allSunk() === true) {
     winner = player2;
+    gameStateIndicator = 2;
   } else if (player2.board.allSunk() === true) {
     winner = player1;
+    gameStateIndicator = 2;
   }
 }
 
